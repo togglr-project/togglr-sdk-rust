90fde7652d86eb4c33dce5a2806f1a1d79b00b05
Roman Chudov <roman.chudov@gmail.com>
2025-10-18 12:10:32 +0300

    v1.1.0

--- generated/src/apis/default_api.rs
+++ generated/src/apis/default_api.rs
@@ -75,9 +75,9 @@ pub enum TrackFeatureEventError {
 
 pub async fn get_feature_health(configuration: &configuration::Configuration, feature_key: &str) -> Result<models::FeatureHealth, Error<GetFeatureHealthError>> {
     // add a prefix to parameters to efficiently prevent name collisions
-    let p_feature_key = feature_key;
+    let p_path_feature_key = feature_key;
 
-    let uri_str = format!("{}/sdk/v1/features/{feature_key}/health", configuration.base_path, feature_key=crate::apis::urlencode(p_feature_key));
+    let uri_str = format!("{}/sdk/v1/features/{feature_key}/health", configuration.base_path, feature_key=crate::apis::urlencode(p_path_feature_key));
     let mut req_builder = configuration.client.request(reqwest::Method::GET, &uri_str);
 
     if let Some(ref user_agent) = configuration.user_agent {
@@ -119,10 +119,10 @@ pub async fn get_feature_health(configuration: &configuration::Configuration, fe
 
 pub async fn report_feature_error(configuration: &configuration::Configuration, feature_key: &str, feature_error_report: models::FeatureErrorReport) -> Result<(), Error<ReportFeatureErrorError>> {
     // add a prefix to parameters to efficiently prevent name collisions
-    let p_feature_key = feature_key;
-    let p_feature_error_report = feature_error_report;
+    let p_path_feature_key = feature_key;
+    let p_body_feature_error_report = feature_error_report;
 
-    let uri_str = format!("{}/sdk/v1/features/{feature_key}/report-error", configuration.base_path, feature_key=crate::apis::urlencode(p_feature_key));
+    let uri_str = format!("{}/sdk/v1/features/{feature_key}/report-error", configuration.base_path, feature_key=crate::apis::urlencode(p_path_feature_key));
     let mut req_builder = configuration.client.request(reqwest::Method::POST, &uri_str);
 
     if let Some(ref user_agent) = configuration.user_agent {
@@ -136,7 +136,7 @@ pub async fn report_feature_error(configuration: &configuration::Configuration, 
         };
         req_builder = req_builder.header("Authorization", value);
     };
-    req_builder = req_builder.json(&p_feature_error_report);
+    req_builder = req_builder.json(&p_body_feature_error_report);
 
     let req = req_builder.build()?;
     let resp = configuration.client.execute(req).await?;
@@ -155,10 +155,10 @@ pub async fn report_feature_error(configuration: &configuration::Configuration, 
 /// Returns feature evaluation result for given project and context. The project is derived from the API key. 
 pub async fn sdk_v1_features_feature_key_evaluate_post(configuration: &configuration::Configuration, feature_key: &str, request_body: std::collections::HashMap<String, serde_json::Value>) -> Result<models::EvaluateResponse, Error<SdkV1FeaturesFeatureKeyEvaluatePostError>> {
     // add a prefix to parameters to efficiently prevent name collisions
-    let p_feature_key = feature_key;
-    let p_request_body = request_body;
+    let p_path_feature_key = feature_key;
+    let p_body_request_body = request_body;
 
-    let uri_str = format!("{}/sdk/v1/features/{feature_key}/evaluate", configuration.base_path, feature_key=crate::apis::urlencode(p_feature_key));
+    let uri_str = format!("{}/sdk/v1/features/{feature_key}/evaluate", configuration.base_path, feature_key=crate::apis::urlencode(p_path_feature_key));
     let mut req_builder = configuration.client.request(reqwest::Method::POST, &uri_str);
 
     if let Some(ref user_agent) = configuration.user_agent {
@@ -172,7 +172,7 @@ pub async fn sdk_v1_features_feature_key_evaluate_post(configuration: &configura
         };
         req_builder = req_builder.header("Authorization", value);
     };
-    req_builder = req_builder.json(&p_request_body);
+    req_builder = req_builder.json(&p_body_request_body);
 
     let req = req_builder.build()?;
     let resp = configuration.client.execute(req).await?;
@@ -236,10 +236,10 @@ pub async fn sdk_v1_health_get(configuration: &configuration::Configuration, ) -
 /// Send a feedback event related to a feature evaluation. Events are written to TimescaleDB (hypertable) and used for analytics, auto-disable and training MAB algorithms. The project is derived from the API key. 
 pub async fn track_feature_event(configuration: &configuration::Configuration, feature_key: &str, track_request: models::TrackRequest) -> Result<(), Error<TrackFeatureEventError>> {
     // add a prefix to parameters to efficiently prevent name collisions
-    let p_feature_key = feature_key;
-    let p_track_request = track_request;
+    let p_path_feature_key = feature_key;
+    let p_body_track_request = track_request;
 
-    let uri_str = format!("{}/sdk/v1/features/{feature_key}/track", configuration.base_path, feature_key=crate::apis::urlencode(p_feature_key));
+    let uri_str = format!("{}/sdk/v1/features/{feature_key}/track", configuration.base_path, feature_key=crate::apis::urlencode(p_path_feature_key));
     let mut req_builder = configuration.client.request(reqwest::Method::POST, &uri_str);
 
     if let Some(ref user_agent) = configuration.user_agent {
@@ -253,7 +253,7 @@ pub async fn track_feature_event(configuration: &configuration::Configuration, f
         };
         req_builder = req_builder.header("Authorization", value);
     };
-    req_builder = req_builder.json(&p_track_request);
+    req_builder = req_builder.json(&p_body_track_request);
 
     let req = req_builder.build()?;
     let resp = configuration.client.execute(req).await?;
